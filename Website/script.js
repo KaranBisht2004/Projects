22268f80b4aab9d59118ae36faf00c761fee565c
KaranBisht2004 <kxrxnbisht@gmail.com>
2026-07-26 20:45:24 +0530

    script.js

--- Website/script.js
+++ Website/script.js
@@ -1,5 +1,26 @@
-// const scroll = new LocomotiveScroll({
-//     el: document.querySelector('#main'),
-//     smooth: true
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
 
-// });
+});
+
+const elementContainer = document.getElementById("elementContainer");
+const fixedImg = document.getElementById("fixed-image");
+const elements = document.querySelectorAll(".element");
+
+elementContainer.addEventListener("mouseenter", () => {
+    if (fixedImg) fixedImg.style.display = "block";
+});
+
+elementContainer.addEventListener("mouseleave", () => {
+    if (fixedImg) fixedImg.style.display = "none";
+});
+
+elements.forEach((element) => {
+    element.addEventListener("mouseenter", () => {
+        const imageSrc = element.getAttribute("data-img");
+        if (fixedImg) {
+            fixedImg.style.backgroundImage = `url("${imageSrc}")`;
+        }
+    });
+});
